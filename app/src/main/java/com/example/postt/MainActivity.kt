d05404ae7e974923c52b32451241f58f19329c11
shevartv <shevartov@gmail.com>
2021-06-22 12:32:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/postt/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.postt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+    }
+}
